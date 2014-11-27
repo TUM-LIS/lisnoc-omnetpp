7ffdd40d69ac403ea7cdf9f0641475d4964e787a
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2014-11-27 11:58:33 +0100

    Heatmap: Match output port also

--- simulations/scripts/GenerateHeatmap.py
+++ simulations/scripts/GenerateHeatmap.py
@@ -26,6 +26,12 @@ for i in files:
             if idx not in heatcollect:
                 heatcollect[idx] = []
             heatcollect[idx].append(float(m.group(4)))
+        m = re.search("router\[(\d+)\].statisticsUnit,outBuffer_(\d+)_vc_(\d+)_latency:mean,(\d+(\.\d+)?)", l)
+        if m:
+            idx = int(m.group(1))
+            if idx not in heatcollect:
+                heatcollect[idx] = []
+            heatcollect[idx].append(float(m.group(4)))
 
     for idx in heatcollect.keys():
         heat[idx] = sum(heatcollect[idx])/len(heatcollect[idx])
