f12974c365300987c22a30b0d3ba5992d125295d
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2014-11-27 11:51:06 +0100

    Generate heatmap
    The script iterates a list of files and outputs the heatmap of all
    routers for each file in each one line. The files are either given as
    parameter or all csv files in the current folder are used if no
    parameter is given.
    
    The heatmap is a dictionary with router ids and the mean service time
    at all buffers in the routers.

---
+++ simulations/scripts/GenerateHeatmap.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import sys, re, os
+
+files = []
+
+if len(sys.argv) < 2:
+    for l in os.listdir("."):
+        if re.match("(\w+)-(\d+).csv", l):
+            files.append(l)
+else:
+    files = sys.argv[1:]
+
+files.sort()
+
+for i in files:
+    f = open(i)
+
+    heatcollect = {}
+    heat = {}
+
+    for l in f:
+        m = re.search("router\[(\d+)\].statisticsUnit,inBuffer_(\d+)_vc_(\d+)_latency:mean,(\d+\.\d+)", l)
+        if m:
+            idx = int(m.group(1))
+            if idx not in heatcollect:
+                heatcollect[idx] = []
+            heatcollect[idx].append(float(m.group(4)))
+
+    for idx in heatcollect.keys():
+        heat[idx] = sum(heatcollect[idx])/len(heatcollect[idx])
+
+    print heat
