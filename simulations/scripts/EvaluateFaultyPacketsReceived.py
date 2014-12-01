6d064ec7f57157a28477d4a2b05a317cd4df7c66
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2014-12-01 11:08:24 +0100

    Make fault packets script a module

--- simulations/scripts/EvaluateFaultyPacketsReceived.py
+++ simulations/scripts/EvaluateFaultyPacketsReceived.py
@@ -2,29 +2,42 @@
 
 import sys, re, os
 
-files = []
-
-if len(sys.argv) < 2:
-    for l in os.listdir("."):
-        if re.match("(\w+)-(\d+).csv", l):
-            files.append(l)
-else:
-    files = sys.argv[1:]
-
-files.sort()
-
-for i in files:
-    f = open(i)
-
+def evaluate(file):
     total = 0
+    faulty = 0
+
+    f = open(file)
 
     for l in f:
-        m = re.search("ni\[(\d+)\].statisticsUnit,packet_arrived_faulty:sum,(\d+(.(\d+)e\+(\d+))?)", l)
+        m = re.search("ni\[(\d+)\].statisticsUnit,packet_arrived_faulty:count,(\d+(.(\d+)e\+(\d+))?)", l)
         if m:
             if m.group(3):
                 raise(BaseException("ERROR: we need precise numbers"))
             else:
                 total = total + int(m.group(2))
 
-    print total
+        m = re.search("ni\[(\d+)\].statisticsUnit,packet_arrived_faulty:sum,(\d+(.(\d+)e\+(\d+))?)", l)
+        if m:
+            if m.group(3):
+                raise(BaseException("ERROR: we need precise numbers"))
+            else:
+                faulty = faulty + int(m.group(2))
+    
+    return { 'total': total, 'faulty': faulty }
+
+if __name__ == "__main__":
+    files = []
+
+    if len(sys.argv) < 2:
+        for l in os.listdir("."):
+            if re.match("(\w+)-(\d+).csv", l):
+                files.append(l)
+    else:
+        files = sys.argv[1:]
+
+    files.sort()
+
+    for i in files:
+        print evaluate(i)
+
 
